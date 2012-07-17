d4630aad72bb083f70f134e7eec6f693432c2814
Hidayet Doğan <hdogan@gmail.com>
2012-07-17 13:14:41 +0300

    SQL injection duzeltme ve kodlama standartlari duzeltmeleri

--- forgot.php
+++ forgot.php
@@ -8,6 +8,7 @@ connect_db();
 if ($_POST['action'] == 'send_mail' && $_POST['email']) {
     $email = mysql_real_escape_string($_POST['email']);
     $query = mysql_query("SELECT id FROM users WHERE email = '$email'");
+
     if (mysql_num_rows($query) > 0) {
         $user_id  = mysql_result($query, 0);
         $password = substr(md5(rand()), 0, 6);
